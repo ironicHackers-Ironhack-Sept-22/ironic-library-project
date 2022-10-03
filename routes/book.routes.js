e0dd3a5f3a00c5688fa34db8c4a70ce4a9fec1d4
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-10-03 17:10:44 +0200

    Step 6: (DELETE) functionality to delete a book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -93,5 +93,17 @@ router.post("/books/:bookId/edit", (req, res, next) => {
 });
 
 
+//DELETE
+router.post("/books/:bookId/delete", (req, res, next) => {
+  Book.findByIdAndDelete(req.params.bookId)
+    .then(() => {
+      res.redirect("/books");
+    })
+    .catch(err => {
+      console.log("Error deleting book...", err);
+      next();
+    });
+
+});
 
 module.exports = router;
