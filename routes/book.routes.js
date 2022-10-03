766882781e2effbcc8136990223f226dc2559cbb
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-10-03 14:58:17 +0200

    Step 3: (READ) book details page

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -2,13 +2,34 @@ const Book = require("../models/Book.model");
 
 const router = require("express").Router();
 
-
+//List all books
 router.get("/books", (req, res, next) => {
   Book.find()
     .then( booksFromDB => {
         res.render("books/books-list", {books: booksFromDB})
     })
-    .catch( err => console.log("error getting books from DB", err))
+    .catch( err => {
+      console.log("error getting books from DB", err);
+      next();
+    })
 });
+
+
+//Book details
+router.get("/books/:bookId", (req, res, next) => {
+  const id = req.params.bookId;
+
+  Book.findById(id)
+    .then( bookDetails => {
+      res.render("books/book-details", bookDetails);
+    } )
+    .catch( err => {
+      console.log("error getting book details from DB", err);
+      next();
+    })
+});
+
+
+
 
 module.exports = router;
