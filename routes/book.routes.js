ba9ce1921fa3f9ea824a0d315fbb17875d886531
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-10-03 17:05:18 +0200

    Step 5: (UPDATE) functionality to update a book

--- routes/book.routes.js
+++ routes/book.routes.js
@@ -58,4 +58,40 @@ router.post("/books/create", (req, res, next) => {
 })
 
 
+//UPDATE: display form
+router.get("/books/:bookId/edit", (req, res, next) => {
+  Book.findById(req.params.bookId)
+    .then( (bookDetails) => {
+      res.render("books/book-edit", bookDetails);
+    })
+    .catch( err => {
+      console.log("Error getting book details from DB...", err);
+      next();
+    });
+});
+
+
+//UPDATE: process form
+router.post("/books/:bookId/edit", (req, res, next) => {
+  const bookId = req.params.bookId;
+
+  const newDetails = {
+    title: req.body.title,
+    author: req.body.author,
+    description: req.body.description,
+    rating: req.body.rating,
+  }
+
+  Book.findByIdAndUpdate(bookId, newDetails)
+    .then(() => {
+      res.redirect(`/books/${bookId}`);
+    })
+    .catch(err => {
+      console.log("Error updating book...", err);
+      next();
+    });
+});
+
+
+
 module.exports = router;
