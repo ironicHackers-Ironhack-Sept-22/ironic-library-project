93044121b0e811191ba62c3fbe9ff8e764e313ba
Luis Junco <38411763+ludovico-wd@users.noreply.github.com>
2022-10-04 15:20:32 +0200

    functionality to display list of authors

---
+++ routes/author.routes.js
@@ -0,0 +1,19 @@
+const Author = require("../models/Author.model");
+
+const router = require("express").Router();
+
+
+//READ: List all authors
+router.get("/authors", (req, res, next) => {
+  Author.find()
+    .then(authors => {
+      res.render("authors/authors-list", { authors });
+    })
+    .catch(err => {
+      console.log('Error getting authors from DB...', err);
+      next(err);
+    })
+});
+
+
+module.exports = router;
